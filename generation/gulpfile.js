95c54c177185b0ba4f5cc5daf256cd367064b2bb
Andrii Malytskyi <a.malytskyi@gmail.com>
2022-08-23 20:43:19 +0000

    fix: replace gulp target to new structure
    - plus remove copy-to-target

--- generation/gulpfile.js
+++ generation/gulpfile.js
@@ -19,24 +19,24 @@ gulp.task('pug', function () {
         .pipe(pug({
             locals: {}
         }))
-        .pipe(gulp.dest('../../../target/static-resources/app/'))
+        .pipe(gulp.dest('../webapp/app/'))
 });
 
 gulp.task('sass', function () {
     return gulp.src('./assets/**/*.sass')
         .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('../../../target/static-resources/app/'));
+        .pipe(gulp.dest('../webapp/app/'));
 });
 gulp.task('autoprefixer', function () {
-    return gulp.src('../../../target/static-resources/app/style/main.css')
+    return gulp.src('../webapp/app/style/main.css')
         .pipe(autoprefixer({}))
-        .pipe(gulp.dest('../../../target/static-resources/app/style/'));
+        .pipe(gulp.dest('../webapp/app/style/'));
 });
 gulp.task('css-concat', function () {
-    return gulp.src(['../../../target/static-resources/app/style/sprite.css', '../../../target/static-resources/app/style/main.css'])
+    return gulp.src(['../webapp/app/style/sprite.css', '../webapp/app/style/main.css'])
         .pipe(concat('app.css'))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('../../../target/static-resources/app/style/'))
+        .pipe(gulp.dest('../webapp/app/style/'))
 });
 gulp.task('css-build', gulp.series('sass', 'autoprefixer', 'css-concat'));
 gulp.task('css-third-party', function () {
@@ -46,15 +46,15 @@ gulp.task('css-third-party', function () {
         './node_modules/font-awesome/css/font-awesome.css'
     ])
         .pipe(concat('source.css'))
-        .pipe(gulp.dest('../../../target/static-resources/app/third-party/style'));
+        .pipe(gulp.dest('../webapp/app/third-party/style'));
 });
 
 gulp.task('css-third-party-resources:font-awesome', function () {
-    return gulp.src('./node_modules/font-awesome/fonts/*').pipe(gulp.dest('../../../target/static-resources/app/third-party/fonts'));
+    return gulp.src('./node_modules/font-awesome/fonts/*').pipe(gulp.dest('../webapp/app/third-party/fonts'));
 });
 
 gulp.task('css-third-party-resources:lato', function () {
-    return gulp.src('./node_modules/lato/font/**/*').pipe(gulp.dest('../../../target/static-resources/app/third-party/font'));
+    return gulp.src('./node_modules/lato/font/**/*').pipe(gulp.dest('../webapp/app/third-party/font'));
 });
 
 gulp.task('css-third-party-resources', gulp.series('css-third-party-resources:font-awesome', 'css-third-party-resources:lato'));
@@ -63,15 +63,15 @@ gulp.task('css', gulp.series('css-build', 'css-third-party', 'css-third-party-re
 
 gulp.task('copy-images', function () {
     return gulp.src(['./assets/**/*.png', './assets/**/*.jpg'])
-        .pipe(gulp.dest('../../../target/static-resources/app/'));
+        .pipe(gulp.dest('../webapp/app/'));
 });
 gulp.task('sprites', function () {
     return gulp.src('./assets/**/sprite_*.{png,jpg}').pipe(spritesmith({
         imgName: '../images/sprite.png',
         cssName: 'sprite.css'
     })).pipe(gulpif('*.png',
-        gulp.dest('../../../target/static-resources/app/images'),
-        gulp.dest('../../../target/static-resources/app/style/'))
+        gulp.dest('../webapp/app/images'),
+        gulp.dest('../webapp/app/style/'))
     );
 });
 gulp.task('images', gulp.series('copy-images', 'sprites'));
@@ -103,7 +103,7 @@ gulp.task('lint', function () {
 });
 gulp.task('copy-js', function () {
     return gulp.src('./assets/**/*.js')
-        .pipe(gulp.dest('../../../target/static-resources/app/'));
+        .pipe(gulp.dest('../webapp/app/'));
 });
 gulp.task('minify', function () {
     return gulp.src('./assets/**/*.js')
@@ -114,7 +114,7 @@ gulp.task('minify', function () {
             mangle: false // otherwhise the sourcemap/debugger does not work properly.
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('../../../target/static-resources/app/js/'));
+        .pipe(gulp.dest('../webapp/app/js/'));
 });
 
 gulp.task('js-build', gulp.series('lint', 'copy-js', 'minify'));
@@ -136,25 +136,20 @@ gulp.task('js-third-party', function () {
             }
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('../../../target/static-resources/app/third-party/'));
+        .pipe(gulp.dest('../webapp/app/third-party/'));
 });
 gulp.task('js', gulp.series('js-test', 'js-build', 'js-third-party'));
 
 gulp.task('clean', function (cb) {
-    return del(['../../../target/static-resources/', '../../../target/apache-tomee/webapps/ROOT/app/'], {
+    return del(['../webapp/app/'], {
         force: true
     }, cb);
 });
 
-gulp.task('copy-to-target', function () {
-    return gulp.src('../../../target/static-resources/app/**/*')
-        .pipe(gulp.dest('../../../target/apache-tomee/webapps/' + applicationContext + '/app/'));
-});
-
 gulp.task('build', gulp.series('clean', 'pug', 'images', 'css', 'js'));
-gulp.task('default', gulp.series('build', 'copy-to-target'), function () {
+gulp.task('default', gulp.series('build'), function () {
     gulp.watch(
         ['./assets/**/*', '../../test/**/*.js'],
-        gulp.series('build', 'copy-to-target')
+        gulp.series('build')
     );
 });
