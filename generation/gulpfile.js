0db3096cddf748200505bf78b31a8e9bc9c6368a
Andrii Malytskyi <a.malytskyi@gmail.com>
2022-08-23 23:48:35 +0000

    feat: update depdendencies and clean gulp script

--- generation/gulpfile.js
+++ generation/gulpfile.js
@@ -1,18 +1,19 @@
-var applicationContext = 'ROOT';
+import gulp from 'gulp';
+import pug from 'gulp-pug';
+import gultSass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import cleanCSS from 'gulp-clean-css';
+import minify from 'gulp-uglify'; // Can to be replaced by `gulp-terser` for es6+
+import sourcemaps from 'gulp-sourcemaps';
+import concat from 'gulp-concat';
+import { deleteAsync } from 'del';
+import spritesmith from 'gulp.spritesmith';
+import gulpif from 'gulp-if';
+import jslint from 'gulp-jslint';
+import dartSass from 'sass';
+//import util from 'gulp-util';
 
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var cleanCSS = require('gulp-clean-css');
-var minify = require('gulp-uglify'); // Can to be replaced by `gulp-terser` for es6+
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var del = require('del');
-var spritesmith = require('gulp.spritesmith');
-var gulpif = require('gulp-if');
-var jslint = require('gulp-jslint');
-var KarmaServer = require('karma').Server;
+const sass = gultSass(dartSass);
 
 gulp.task('pug', function () {
     return gulp.src('./assets/**/*.pug')
@@ -24,7 +25,7 @@ gulp.task('pug', function () {
 
 gulp.task('sass', function () {
     return gulp.src('./assets/**/*.sass')
-        .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass.sync({outputStyle: 'compressed', quietDeps: true}).on('error', sass.logError))
         .pipe(gulp.dest('../webapp/app/'));
 });
 gulp.task('autoprefixer', function () {
@@ -76,15 +77,6 @@ gulp.task('sprites', function () {
 });
 gulp.task('images', gulp.series('copy-images', 'sprites'));
 
-gulp.task('js-test', function (done) {
-    new KarmaServer({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true,
-        autoWatch: false
-    }, function () {
-        done(); // avoiding karma to shutdown watch on test failures.
-    }).start();
-});
 gulp.task('lint', function () {
     return gulp.src('./assets/**/*.js').pipe(jslint({
             node: false,
@@ -138,18 +130,25 @@ gulp.task('js-third-party', function () {
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('../webapp/app/third-party/'));
 });
-gulp.task('js', gulp.series('js-test', 'js-build', 'js-third-party'));
+gulp.task('js', gulp.series('js-build', 'js-third-party'));
 
-gulp.task('clean', function (cb) {
-    return del(['../webapp/app/'], {
+gulp.task('clean', function (done) {
+    return deleteAsync(['../webapp/app/'], {
         force: true
-    }, cb);
+    }).then(res => {
+        if (res?.length) {
+            //util.log(`Cleaned ${res.join(',')}`);
+        }
+        done();
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch(
+        ['./assets/**/*'],
+        gulp.series('build')
+    )
 });
 
 gulp.task('build', gulp.series('clean', 'pug', 'images', 'css', 'js'));
-gulp.task('default', gulp.series('build'), function () {
-    gulp.watch(
-        ['./assets/**/*', '../../test/**/*.js'],
-        gulp.series('build')
-    );
-});
+gulp.task('default', gulp.parallel('build', 'watch'));
