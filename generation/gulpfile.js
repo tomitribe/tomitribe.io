9c9db16036470804d170248beb44c4f19970fe32
Andrii Malytskyi <a.malytskyi@gmail.com>
2023-01-15 19:17:04 -0500

    fix: gh pages seo sitemap

--- generation/gulpfile.js
+++ generation/gulpfile.js
@@ -13,51 +13,72 @@ import gulpESLint from 'gulp-eslint-new';
 import dartSass from 'sass';
 import rename from 'gulp-rename';
 import replace from 'gulp-replace';
+import sitemap from 'gulp-sitemap';
+import save from 'gulp-save';
+import { execSync } from 'child_process';
 //import util from 'gulp-util';
 
 const sass = gultSass(dartSass);
+const webappPath = '../webapp';
+const staticPath = '../static';
+const assets = './assets';
+const node_modules = './node_modules';
+const siteUrl = 'https://tomitribe.io';
+
+// get lastmodified from git
+const lastmod = (file) => execSync(`git log -1 --pretty="format:%ci" ${file.path}`);
+
+gulp.task('sitemap', function () {
+    return gulp.src([`${staticPath}/**/index.html`], {read: false})
+        .pipe(sitemap({siteUrl, lastmod}))
+        .pipe(save('clone-sitemap'))
+        .pipe(gulp.dest(`${staticPath}`))
+        .pipe(save.restore('clone-sitemap'))
+        .pipe(replace(/<loc>https:\/\/tomitribe.io\/(\w+)/mgi, `<loc>${siteUrl}/?/$1`))
+        .pipe(gulp.dest(`${webappPath}`));
+});
 
 gulp.task('pug', function () {
-    return gulp.src('./assets/**/*.pug')
+    return gulp.src(`${assets}/**/*.pug`)
         .pipe(pug({
             locals: {}
         }))
-        .pipe(gulp.dest('../webapp/app/'))
+        .pipe(gulp.dest(`${webappPath}/app/`))
 });
 
 gulp.task('sass', function () {
-    return gulp.src('./assets/**/*.sass')
+    return gulp.src(`${assets}/**/*.sass`)
         .pipe(sass.sync({outputStyle: 'compressed', quietDeps: true}).on('error', sass.logError))
-        .pipe(gulp.dest('../webapp/app/'));
+        .pipe(gulp.dest(`${webappPath}/app/`));
 });
 gulp.task('autoprefixer', function () {
-    return gulp.src('../webapp/app/style/main.css')
+    return gulp.src(`${webappPath}/app/style/main.css`)
         .pipe(autoprefixer({}))
-        .pipe(gulp.dest('../webapp/app/style/'));
+        .pipe(gulp.dest(`${webappPath}/app/style/`));
 });
 gulp.task('css-concat', function () {
-    return gulp.src(['../webapp/app/style/sprite.css', '../webapp/app/style/main.css'])
+    return gulp.src([`${webappPath}/app/style/sprite.css`, `${webappPath}/app/style/main.css`])
         .pipe(concat('app.css'))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('../webapp/app/style/'))
+        .pipe(gulp.dest(`${webappPath}/app/style/`))
 });
 gulp.task('css-build', gulp.series('sass', 'autoprefixer', 'css-concat'));
 gulp.task('css-third-party', function () {
     return gulp.src([
-        './node_modules/lato/css/lato.css',
-        './node_modules/normalize.css/normalize.css',
-        './node_modules/font-awesome/css/font-awesome.css'
+        `${node_modules}/lato/css/lato.css`,
+        `${node_modules}/normalize.css/normalize.css`,
+        `${node_modules}/font-awesome/css/font-awesome.css`
     ])
         .pipe(concat('source.css'))
-        .pipe(gulp.dest('../webapp/app/third-party/style'));
+        .pipe(gulp.dest(`${webappPath}/app/third-party/style`));
 });
 
 gulp.task('css-third-party-resources:font-awesome', function () {
-    return gulp.src('./node_modules/font-awesome/fonts/*').pipe(gulp.dest('../webapp/app/third-party/fonts'));
+    return gulp.src(`${node_modules}/font-awesome/fonts/*`).pipe(gulp.dest(`${webappPath}/app/third-party/fonts`));
 });
 
 gulp.task('css-third-party-resources:lato', function () {
-    return gulp.src('./node_modules/lato/font/**/*').pipe(gulp.dest('../webapp/app/third-party/font'));
+    return gulp.src(`${node_modules}/lato/font/**/*`).pipe(gulp.dest(`${webappPath}/app/third-party/font`));
 });
 
 gulp.task('css-third-party-resources', gulp.series('css-third-party-resources:font-awesome', 'css-third-party-resources:lato'));
@@ -65,22 +86,22 @@ gulp.task('css', gulp.series('css-build', 'css-third-party', 'css-third-party-re
 
 
 gulp.task('copy-images', function () {
-    return gulp.src(['./assets/**/*.png', './assets/**/*.jpg'])
-        .pipe(gulp.dest('../webapp/app/'));
+    return gulp.src([`${assets}/**/*.png`, `${assets}/**/*.jpg`])
+        .pipe(gulp.dest(`${webappPath}/app/`));
 });
 gulp.task('sprites', function () {
-    return gulp.src('./assets/**/sprite_*.{png,jpg}').pipe(spritesmith({
+    return gulp.src(`${assets}/**/sprite_*.{png,jpg}`).pipe(spritesmith({
         imgName: '../images/sprite.png',
         cssName: 'sprite.css'
     })).pipe(gulpif('*.png',
-        gulp.dest('../webapp/app/images'),
-        gulp.dest('../webapp/app/style/'))
+        gulp.dest(`${webappPath}/app/images`),
+        gulp.dest(`${webappPath}/app/style/`))
     );
 });
 gulp.task('images', gulp.series('copy-images', 'sprites'));
 
 gulp.task('lint', function () {
-    return gulp.src('./assets/**/*.js')
+    return gulp.src(`${assets}/**/*.js`)
     .pipe(gulpESLint({
         fix: true,
         warnIgnored: true
@@ -89,11 +110,11 @@ gulp.task('lint', function () {
     .pipe(gulpESLint.format());
 });
 gulp.task('copy-js', function () {
-    return gulp.src('./assets/**/*.js')
-        .pipe(gulp.dest('../webapp/app/'));
+    return gulp.src(`${assets}/**/*.js`)
+        .pipe(gulp.dest(`${webappPath}/app/`));
 });
 gulp.task('copy-404', function () {
-    return gulp.src('../webapp/index.html')
+    return gulp.src(`${webappPath}/index.html`)
         .pipe(rename(function (path) {
             path.basename = "404";
         }))
@@ -101,11 +122,11 @@ gulp.task('copy-404', function () {
             replace(/<ng-view.[\w\W]*<\/body>/mgi,
         `<script>window.location.replace(location.href.replace(document.baseURI, '?/'));</script>
 </body>`))
-        .pipe(gulp.dest('../webapp'));
+        .pipe(gulp.dest(`${webappPath}`));
 });
 
 gulp.task('minify', function () {
-    return gulp.src('./assets/**/*.js')
+    return gulp.src(`${assets}/**/*.js`)
         .pipe(sourcemaps.init())
         .pipe(concat('app.min.js'))
         .pipe(minify({
@@ -113,17 +134,17 @@ gulp.task('minify', function () {
             mangle: false // otherwhise the sourcemap/debugger does not work properly.
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('../webapp/app/js/'));
+        .pipe(gulp.dest(`${webappPath}/app/js/`));
 });
 
 gulp.task('js-build', gulp.series('lint', 'copy-js', 'minify'));
 
 gulp.task('js-third-party', function () {
     return gulp.src([
-        './node_modules/underscore/underscore.js',
-        './node_modules/jquery/dist/jquery.js',
-        './node_modules/angular/angular.js',
-        './node_modules/angular-route/angular-route.js'
+        `${node_modules}/underscore/underscore.js`,
+        `${node_modules}/jquery/dist/jquery.js`,
+        `${node_modules}/angular/angular.js`,
+        `${node_modules}/angular-route/angular-route.js`
     ])
         .pipe(sourcemaps.init())
         .pipe(concat('source.js'))
@@ -135,12 +156,12 @@ gulp.task('js-third-party', function () {
             }
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('../webapp/app/third-party/'));
+        .pipe(gulp.dest(`${webappPath}/app/third-party/`));
 });
 gulp.task('js', gulp.series('js-build', 'js-third-party'));
 
 gulp.task('clean', function (done) {
-    return deleteAsync(['../webapp/app/', '../webapp/404.html'], {
+    return deleteAsync([`${webappPath}/app/`, `${webappPath}/404.html`], {
         force: true
     }).then(res => {
         if (res?.length) {
@@ -152,7 +173,7 @@ gulp.task('clean', function (done) {
 
 gulp.task('watch', function () {
     gulp.watch(
-        ['./assets/**/*'],
+        [`${assets}/**/*`],
         gulp.series('build')
     )
 });
